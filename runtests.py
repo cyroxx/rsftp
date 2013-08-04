464032a899e56ed70171b0b6a694150085c2aa1d
Matthias Jacob <dev@matthiasjacob.net>
2013-08-04 15:38:28 +0200

    show errors and failures for tests

--- runtests.py
+++ runtests.py
@@ -1,13 +1,17 @@
 from twisted.trial.runner import TestLoader
-from twisted.trial.reporter import *
+from twisted.trial.reporter import VerboseTextReporter
 
-def pe(reporter):
-    for target, failure in reporter.errors:
-        failure.printTraceback()
+def printProblems(reporter):
+    for problems in [reporter.errors, reporter.failures]:
+        for target, failure in problems:
+            print '-' * 50
+            print 'Target:', target
+            print ''
+            failure.printTraceback()
 
 loader = TestLoader()
 module = loader.findByName('test')
 suite = loader.loadModule(module)
 reporter = suite.run(VerboseTextReporter())
 
-pe(reporter)
+printProblems(reporter)
