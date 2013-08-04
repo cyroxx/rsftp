908927c7f36726979af2667011fc8ff5c2cd67a3
Matthias Jacob <dev@matthiasjacob.net>
2013-08-04 17:21:06 +0200

    remove a lot of unused code

--- rs/filepath.py
+++ rs/filepath.py
@@ -59,82 +59,6 @@ class RSFilePath(object):
         d.addCallback(cbGetChildInfo)
         return d
 
-#        path = ''.join([self.path, name, sep])
-#
-#        def gotResponse(response):
-#            return self._handleResponse(response, path)
-#
-#        d = treq.get(path)
-#        d.addCallback(gotResponse)
-#
-#        def cbHandleResponse(response):
-#            print "child ", path
-#            return RSFilePath.clone(path)
-#
-#        def ebHandleResponse(failure):
-#            raise InsecurePath("%r is not a child of %s" % (path, self.path))
-#            #return failure
-#
-#        d.addCallbacks(cbHandleResponse, ebHandleResponse)
-
-
-#        myd = defer.Deferred()
-#        myd.errback(NotFoundError)
-#        return d
-        #raise NotImplementedError
-        #return d
-
-    def open(self, mode="r"):
-        """
-        Opens this file path with the given mode.
-
-        @return: a file-like object.
-        @raise Exception: if this file path cannot be opened.
-        """
-        raise NotImplementedError
-
-    def changed(self):
-        """
-        Clear any cached information about the state of this path on disk.
-        """
-        raise NotImplementedError
-
-    def getsize(self):
-        """
-        Retrieve the size of this file in bytes.
-
-        @return: the size of the file at this file path in bytes.
-        @raise Exception: if the size cannot be obtained.
-        """
-        raise NotImplementedError
-
-    def getModificationTime(self):
-        """
-        Retrieve the time of last access from this file.
-
-        @return: a number of seconds from the epoch.
-        @rtype: L{float}
-        """
-        raise NotImplementedError
-
-    def getStatusChangeTime(self):
-        """
-        Retrieve the time of the last status change for this file.
-
-        @return: a number of seconds from the epoch.
-        @rtype: L{float}
-        """
-        raise NotImplementedError
-
-    def getAccessTime(self):
-        """
-        Retrieve the time that this file was last accessed.
-
-        @return: a number of seconds from the epoch.
-        @rtype: L{float}
-        """
-        raise NotImplementedError
-
     def exists(self):
         """
         Check if this file path exists.
@@ -168,42 +92,6 @@ class RSFilePath(object):
         """
         return not self.isdir()
 
-    def children(self):
-        """
-        List the children of this path object.
-
-        @return: a sequence of the children of the directory at this file path.
-        @raise Exception: if the file at this file path is not a directory.
-        """
-        raise NotImplementedError
-
-    def basename(self):
-        """
-        Retrieve the final component of the file path's path (everything
-        after the final path separator).
-
-        @return: the base name of this file path.
-        @rtype: L{str}
-        """
-        raise NotImplementedError
-
-    def parent(self):
-        """
-        A file path for the directory containing the file at this file path.
-        """
-        raise NotImplementedError
-
-    def sibling(self, name):
-        """
-        A file path for the directory containing the file at this file path.
-
-        @param name: the name of a sibling of this path. C{name} must be a direct
-            sibling of this path and may not contain a path separator.
-
-        @return: a sibling file path of this one.
-        """
-        raise NotImplementedError
-
     def listdir(self):
         print "listdir called: ", self.path
 
@@ -236,7 +124,6 @@ class RSFilePath(object):
                       'directory': is_directory,
                       'permissions': 16895 if is_directory else 33206,
                       'hardlinks': 0,
-                      # FIXME: we assume unreasonably that current_version is a numeric timestamp
                       'modified': modified,
                       'owner': '0',
                       'group': '0'}
